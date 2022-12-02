5ec1184e047d1192406379224d9acdac50d6c5f9
JSchoonbrood <schoonbrood.jake@gmail.com>
2022-12-02 15:19:08 +0000

    Huberloss script

---
+++ scripts/huberloss.py
@@ -0,0 +1,19 @@
+"""Contains function to return huberloss"""
+
+import tensorflow as tf
+
+def get_huber_loss_fn(**huber_loss_kwargs):
+    """A function to return huberloss
+    """
+    def custom_huber_loss(y_true, y_pred):
+        """Returns tensorflows huberloss with parameters already passed
+
+        Args:
+            y_true (_type_): y true values
+            y_pred (_type_): y prediction values
+
+        Returns:
+            any: Huberloss function
+        """
+        return tf.compat.v1.losses.huber_loss(y_true, y_pred, **huber_loss_kwargs)
+    return custom_huber_loss
